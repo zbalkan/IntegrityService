8bfccd627d93d620effde49e1abadfb3e9ba0a0b
Zafer Balkan <zafer@zaferbalkan.com>
2022-05-24 11:41:55 +0300

    Add project files.

---
+++ IntegrityService/Program.cs
@@ -0,0 +1,24 @@
+using IntegrityService;
+
+namespace Company.WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .UseWindowsService()
+                .ConfigureLogging(logging
+                    => logging.AddEventLog(conf =>
+                        conf.SourceName = "File Integrity Monitoring Service"
+                        ))
+                .Build();
+
+            host.Run();
+        }
+    }
+}
