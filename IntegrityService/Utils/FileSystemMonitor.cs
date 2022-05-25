72b064fd69f9dc7cd27521e4d807501fbfc9061d
Zafer Balkan <zafer@zaferbalkan.com>
2022-05-25 18:23:13 +0300

    Added basic FileSystem monitoring

---
+++ IntegrityService/Utils/FileSystemMonitor.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace IntegrityService.Utils
+{
+    internal sealed class FileSystemMonitor
+    {
+        private readonly ILogger _logger;
+        private readonly bool _useDigest;
+
+        private List<FileSystemWatcher> watchers;
+
+        public FileSystemMonitor(ILogger logger, bool useDigest)
+        {
+            _logger = logger;
+            _useDigest = useDigest;
+        }
+
+        public void Start() => InvokeWatchers();
+
+        public void Stop()
+        {
+            foreach (var watcher in watchers)
+            {
+                watcher.EnableRaisingEvents = false;
+                watcher.Changed -= OnChanged;
+                watcher.Renamed -= OnChanged;
+                watcher.Created -= OnCreated;
+                watcher.Deleted -= OnDeleted;
+                watcher.Error -= OnError;
+                watcher.Dispose();
+            }
+            watchers.Clear();
+        }
+
+        private void InvokeWatchers()
+        {
+            watchers = new List<FileSystemWatcher>();
+
+            foreach (var path in Settings.Instance.MonitoredPaths)
+            {
+                var watcher = new FileSystemWatcher(path)
+                {
+                    NotifyFilter = NotifyFilters.Attributes
+                                | NotifyFilters.CreationTime
+                                | NotifyFilters.DirectoryName
+                                | NotifyFilters.FileName
+                                // | NotifyFilters.LastAccess // This creates so much bloat.
+                                | NotifyFilters.LastWrite
+                                | NotifyFilters.Security
+                                | NotifyFilters.Size,
+                    IncludeSubdirectories = true,
+                    EnableRaisingEvents = true,
+                    Filter = string.Empty
+                };
+
+                watcher.Changed += OnChanged;
+                watcher.Renamed += OnChanged;
+                watcher.Created += OnCreated;
+                watcher.Deleted += OnDeleted;
+
+                watcher.Error += OnError;
+                watchers.Add(watcher);
+            }
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if ((e.ChangeType != WatcherChangeTypes.Changed && e.ChangeType != WatcherChangeTypes.Renamed) || IsExcluded(e.FullPath))
+            {
+                return;
+            }
+
+            if (_useDigest && IsFile(e.FullPath))
+            {
+                var digest = SHA256CheckSum(e.FullPath);
+                _logger.LogInformation("Changed: {path}\nDigest: {digest}", e.FullPath, digest);
+            }
+            else
+            {
+                _logger.LogInformation("Changed: {path}", e.FullPath);
+            }
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            if (IsExcluded(e.FullPath))
+            {
+                return;
+            }
+
+            if (_useDigest && IsFile(e.FullPath))
+            {
+                var digest = SHA256CheckSum(e.FullPath);
+                _logger.LogInformation("Created: {path}\nDigest: {digest}", e.FullPath, digest);
+            }
+            else
+            {
+                _logger.LogInformation("Created: {path}", e.FullPath);
+            }
+        }
+
+        private void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            if (IsExcluded(e.FullPath))
+            {
+                return;
+            }
+
+            if (_useDigest && IsFile(e.FullPath))
+            {
+                var digest = SHA256CheckSum(e.FullPath);
+                _logger.LogInformation("Delete: {path}\nDigest: {digest}", e.FullPath, digest);
+            }
+            else
+            {
+                _logger.LogInformation("Delete: {path}", e.FullPath);
+            }
+        }
+
+        private void OnError(object sender, ErrorEventArgs e) => PrintException(e.GetException());
+
+        private void PrintException(Exception? ex)
+        {
+            if (ex != null)
+            {
+                var sb = new StringBuilder(120)
+                    .Append("Message: ").AppendLine(ex.Message)
+                    .Append("Stacktrace: ").AppendLine(ex.StackTrace);
+
+                _logger.LogError("Exception: {ex}", sb.ToString());
+
+                PrintException(ex.InnerException);
+            }
+        }
+
+        private static bool IsExcluded(string path)
+        {
+            for (var i = 0; i < Settings.Instance.ExcludedPaths.Count; i++)
+            {
+                if (path.StartsWith(Settings.Instance.ExcludedPaths[i], StringComparison.OrdinalIgnoreCase) ||
+                    path.EndsWith(Settings.Instance.ExcludedPaths[i], StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool IsFile(string fullPath) => File.Exists(fullPath); // If it is a directory or a removed file, you cannot get a digest. So return false.
+
+        private string SHA256CheckSum(string filePath)
+        {
+            var digest = string.Empty;
+
+            try
+            {
+                using var SHA256 = System.Security.Cryptography.SHA256.Create();
+                using var fileStream = File.OpenRead(filePath);
+                digest = Convert.ToHexString(SHA256.ComputeHash(fileStream));
+
+            }
+            catch (Exception ex)
+            {
+                PrintException(ex);
+            }
+            return digest;
+        }
+    }
+}
--- IntegrityService/Worker.cs
+++ IntegrityService/Worker.cs
@@ -1,3 +1,4 @@
+using IntegrityService.Utils;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -9,10 +10,13 @@ namespace IntegrityService
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly FileSystemMonitor _fsMonitor;
 
         public Worker(ILogger<Worker> logger)
         {
             _logger = logger;
+
+            _fsMonitor = new FileSystemMonitor(_logger, true);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -26,11 +30,15 @@ namespace IntegrityService
                 _logger.LogError("Failed to read settings.");
             }
 
+            _fsMonitor.Start();
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(10000, stoppingToken);
+                await Task.Delay(30000, stoppingToken);
             }
+
+            _fsMonitor.Stop();
         }
     }
 }
