e6821f8628a58d86c3be5305a86cb2f0fe278164
Zafer Balkan <zafer@zaferbalkan.com>
2022-06-03 10:54:26 +0300

    Used cancellation tokens for async methods

--- IntegrityService/Worker.cs
+++ IntegrityService/Worker.cs
@@ -1,9 +1,9 @@
-using IntegrityService.Utils;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using IntegrityService.Utils;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace IntegrityService
 {
@@ -36,18 +36,24 @@ namespace IntegrityService
             }
 
             if (!Settings.Instance.DisableLocalDatabase && Registry.ReadDwordValue("FileDiscoveryCompleted") == 0)
-                _backgroundWorkerQueue.QueueBackgroundWorkItem(token => StartFilesystemDiscoveryAsync());
+            {
+                _backgroundWorkerQueue.QueueBackgroundWorkItem(_ => StartFilesystemDiscoveryAsync(stoppingToken));
+            }
 
-            await StartFileMonitoringAsync().ConfigureAwait(false);
+            await StartFileMonitoringAsync(stoppingToken).ConfigureAwait(false);
 
             if (Settings.Instance.EnableRegistryMonitoring)
-                await StartRegistryMonitoringAsync().ConfigureAwait(false);
+            {
+                await StartRegistryMonitoringAsync(stoppingToken).ConfigureAwait(false);
+            }
 
             // This loop must continue until service is stopped.
             while (!stoppingToken.IsCancellationRequested)
             {
                 if (Settings.Instance.HeartbeatInterval >= 0)
+                {
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                }
 
                 var workItem = await _backgroundWorkerQueue.DequeueAsync(stoppingToken);
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
@@ -60,25 +66,36 @@ namespace IntegrityService
             }
         }
 
-        private async Task StartFilesystemDiscoveryAsync() =>
-            await Task.Run(() =>
+        private async Task<Task> StartFilesystemDiscoveryAsync(CancellationToken token)
+        {
+            var tcs = new TaskCompletionSource();
+
+            try
             {
-                _logger.LogInformation(
-                    "Could not find the database file. Initiating file system discovery. It will take time.");
-                Database.Start();
-                Registry.WriteDwordValue("FileDiscoveryCompleted", 0, true);
-                FileSystem.StartSearch(Settings.Instance.MonitoredPaths, Settings.Instance.ExcludedPaths,
-                    Settings.Instance.ExcludedExtensions);
+                await Task.Run(() =>
+                       {
+                           _logger.LogInformation(
+                               "Could not find the database file. Initiating file system discovery. It will take time.");
+                           Database.Start();
+                           Registry.WriteDwordValue("FileDiscoveryCompleted", 0, true);
+                           FileSystem.StartSearch(Settings.Instance.MonitoredPaths, Settings.Instance.ExcludedPaths,
+                               Settings.Instance.ExcludedExtensions);
 
-                Registry.WriteDwordValue("FileDiscoveryCompleted", 1, true);
-                _logger.LogInformation("File system discovery completed.");
-            });
+                           Registry.WriteDwordValue("FileDiscoveryCompleted", 1, true);
+                           _logger.LogInformation("File system discovery completed.");
+                       },
+                       token);
+            }
+            catch (Exception ex)
+            {
+                tcs.SetException(ex);
+            }
+            return tcs.Task;
+        }
 
-        private async Task StartFileMonitoringAsync() =>
-            await Task.Run(() => _fsMonitor.Start()).ConfigureAwait(false);
+        private Task StartFileMonitoringAsync(CancellationToken token) => Task.Run(() => _fsMonitor.Start(), token);
 
-        private async Task StartRegistryMonitoringAsync() =>
-            await Task.Run(() => _regMonitor.Start()).ConfigureAwait(false);
+        private Task StartRegistryMonitoringAsync(CancellationToken token) => Task.Run(() => _regMonitor.Start(), token);
 
         private bool Handler(CtrlType signal)
         {
