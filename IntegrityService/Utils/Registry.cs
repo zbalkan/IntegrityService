af113c2467682389454b85311595a6bee8d9fa1f
Zafer Balkan <zafer@zaferbalkan.com>
2022-06-03 13:34:22 +0300

    Simplified registry key reading

--- IntegrityService/Utils/Registry.cs
+++ IntegrityService/Utils/Registry.cs
@@ -7,36 +7,14 @@ namespace IntegrityService.Utils
 {
     internal static class Registry
     {
-#pragma warning disable CS8601 // Possible null reference assignment.
-        private static readonly RegistryKey HklmSoftware = Microsoft.Win32.Registry.LocalMachine.OpenSubKey("Software", true);
-#pragma warning restore CS8601 // Possible null reference assignment.
-
         private const string FimKeyName = "FIM";
 
-        public static RegistryKey Root
-        {
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-#pragma warning disable CS8603 // Possible null reference return.
-            get
-            {
-                RegistryKey result;
-
-                if (HklmSoftware?.OpenSubKey(FimKeyName, true) == null)
-                {
-                    result = HklmSoftware.CreateSubKey(FimKeyName, true);
-                }
-                else
-                {
-                    result = HklmSoftware.OpenSubKey(FimKeyName, true);
-                }
-
-                return result;
-            }
-#pragma warning restore CS8603 // Possible null reference return.
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+        public static RegistryKey Root => Microsoft.Win32.Registry
+            .LocalMachine
+            .OpenSubKey("Software", true)
+            .OpenSubKey(FimKeyName, true) ?? Microsoft.Win32.Registry.LocalMachine.OpenSubKey("Software", true).CreateSubKey(FimKeyName, true);
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
-        }
 
         public static void WriteMultiStringValue(string value, IEnumerable<string> valueData, bool overwrite = false)
         {
