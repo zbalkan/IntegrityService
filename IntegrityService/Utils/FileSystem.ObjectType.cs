739a56a5ecc24fb6ef1054cad25cadadc02b3cf6
Zafer Balkan <zafer@zaferbalkan.com>
2024-05-06 23:43:22 +0300

    Added filesystem object check to allow hash calculation only for files (#18)

---
+++ IntegrityService/Utils/FileSystem.ObjectType.cs
@@ -0,0 +1,18 @@
+﻿namespace IntegrityService.Utils
+{
+    internal static partial class FileSystem
+    {
+        /// <summary>
+        ///     Filesystem object type.
+        /// </summary>
+        /// <remarks>
+        ///     Hardlinks and junctions are ignored as it does not matter for our application.
+        /// </remarks>
+        public enum ObjectType
+        {
+            Directory,
+            File,
+            SymbolicLink
+        }
+    }
+}
