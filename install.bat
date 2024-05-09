e72fc9565672e520898323164849752ceff649ae
Zafer Balkan <zafer@zaferbalkan.com>
2024-05-09 22:46:34 +0300

    Removed scripts and used only MSI installation options

--- install.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-sc create FIM binpath="C:\Users\zafer\source\repos\IntegrityService\IntegrityService\bin\Release\net6.0-windows\publish\win-x64\IntegrityService.exe" start="demand" displayname="File Integrity Monitoring Service" 
-sc failure FIM reset=0 actions=restart/60000/restart/60000/run/1000
-sc description FIM "File Integrity Monitoring Service"
-sc start FIM
