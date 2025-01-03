430da79389d0dae29f3b161b72351c23d388066a
Zafer Balkan <zafer@zaferbalkan.com>
2025-01-03 14:57:44 +0200

    Updated registry monitoring settings: new paths and enabled by default

--- IntegrityService/Settings.cs
+++ IntegrityService/Settings.cs
@@ -49,17 +49,17 @@ namespace IntegrityService
         public string[] ExcludedPaths { get; private set; }
 
         /// <summary>
+        ///     Ignore caculating hashes of large files for memory consumption.
+        ///     Default: 1024 (1GB)
+        /// </summary>
+        public int HashLimitMB { get; private set; }
+
+        /// <summary>
         ///     Interval in seconds to send an informational heartbeat log entry to allow monitoring
         ///     of the service itself. It can be disabled by setting it 0.
         ///     Default: 60
         /// </summary>
         public int HeartbeatInterval { get; private set; }
-
-        /// <summary>
-        ///     Ignore caculating hashes of large files for memory consumption.
-        ///     Default: 1024 (1GB)
-        /// </summary>
-        public int HashLimitMB { get; private set; }
 
         /// <summary>
         ///     A flag that returns true if file discovery task is completed.
@@ -108,9 +108,9 @@ namespace IntegrityService
         internal static Settings Instance => Lazy.Value;
 #pragma warning restore Ex0101 // Member accessor may throw undocumented exception
 
-        private const int DEFAULT_HEARTBEAT_INTERVAL = 60;
-
         private const int DEFAULT_HASHLIMIT_MB = 1024;
+
+        private const int DEFAULT_HEARTBEAT_INTERVAL = 60;
 
         private static readonly Lazy<Settings> Lazy = new(() => new Settings());
 
@@ -415,18 +415,80 @@ namespace IntegrityService
             var registryMonitoring = Registry.ReadDwordValue("EnableRegistryMonitoring");
             if (registryMonitoring == -1)
             {
-                Registry.WriteDwordValue("EnableRegistryMonitoring", 0);
-                registryMonitoring = 0;
+                Registry.WriteDwordValue("EnableRegistryMonitoring", 1);
+                registryMonitoring = 1;
             }
             EnableRegistryMonitoring = registryMonitoring == 1;
 
             var monitoredKeys = Registry.ReadMultiStringValue("MonitoredKeys");
             if (monitoredKeys.Length == 0)
             {
-                monitoredKeys = [@"HKEY_LOCAL_MACHINE\SOFTWARE\FIM",
-                @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run",
-                @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\RunOnce",
-                @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"];
+                monitoredKeys = [
+                    @"HKEY_LOCAL_MACHINE\SOFTWARE\FIM",
+                    @"HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Microsoft Defender",
+                    @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders",
+                    @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders",
+                    @"HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager",
+                    @"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce",
+                    @"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunServices",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunOnce",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\Windows",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\Run",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunServicesOnce",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\RunServices",
+                    @"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL",
+                    @"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa\FipsAlgorithmPolicy",
+                    @"HKEY_LOCAL_MACHINE\SHKLM\SOFTWARE\Policies\Microsoft\Cryptography\Configuration\SSL\00010002",
+                    @"HKEY_CURRENT_USER\Software\Classes\Mscfile\Shell\Open\Command",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\App Paths\Control.exe",
+                    @"HKEY_CURRENT_USER\Software\Classes\Exefile\Shell\Runas\Command\IsolatedCommand",
+                    @"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows Nt\CurrentVersion\Imagefileexecutionoptions",
+                    @"HKEY_LOCAL_MACHINE\System\CurrentControlSet\Enum\USBTor",
+                    @"HKEY_LOCAL_MACHINE\System\CurrentControlSet\Enum\USB",
+                    @"HKEY_CURRENT_USER\Environment",
+                    @"HKEY_CURRENT_USER\Control Panel\Desktop\Scrnsave.exe",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Command Processor\Autorun",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Desktop\Components",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Explorer Bars",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Extensions",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\UrlSearchHooks\Server\Install\Software\Microsoft\Windows\CurrentVersion\Run",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\Windows\Run",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Winlogon",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\Shell",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Windows NT\CurrentVersion\Run",
+                    @"HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\Control Panel\Desktop\Scrnsave.exe",
+                    @"HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\System\Scripts\Logoff",
+                    @"HKEY_CURRENT_USER\Software\Wow6432Node\Microsoft\Internet Explorer\Explorer Bars",
+                    @"HKEY_CURRENT_USER\Software\Wow6432Node\Microsoft\Internet Explorer\Extensions",
+                    @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Winlogon",
+                    @"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\Notify",
+                    @"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\Shell",
+                    @"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Winlogon",
+                    @"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\System",
+                    @"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows NT\CurrentVersion\Winlogon\Taskman",
+                    @"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\GroupPolicy\Scripts\Shutdown",
+                    @"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\GroupPolicy\Scripts\Startup",
+                    @"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\Run",
+                    @"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System\Shell",
+                    @"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run",
+                    @"HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\RunOnce",
+                    @"HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\System\Scripts\Logoff",
+                    @"HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\System\Scripts\Logon",
+                    @"HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\System\Scripts\Shutdown",
+                    @"HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\System\Scripts\Startup",
+                    @"HKEY_LOCAL_MACHINE\Software\Wow6432Node\Microsoft\Command\Processor\Autorun",
+                    @"HKEY_LOCAL_MACHINE\Software\Wow6432Node\Microsoft\Internet Explorer\Explorer Bars",
+                    @"HKEY_LOCAL_MACHINE\Software\Wow6432Node\Microsoft\Internet Explorer\Extensions",
+                    @"HKEY_LOCAL_MACHINE\Software\Wow6432Node\Microsoft\Internet Explorer\Toolbar",
+                    @"HKEY_LOCAL_MACHINE\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Run",
+                    @"HKEY_LOCAL_MACHINE\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\RunOnce",
+                    @"HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\LSA",
+                    @"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layout",
+                    @"HKEY_CURRENT_USER\Keyboard Layout\Preload"];
+
                 Registry.WriteMultiStringValue("MonitoredKeys", monitoredKeys);
             }
             MonitoredKeys = monitoredKeys.Order().ToArray();
